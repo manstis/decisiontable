816abc4da50f1a2226e94fc144dda04908d0bdbd
Michael Anstis <manstis@redhat.com>
2010-11-29 16:45:43 +0000

    Preliminaries for sorting...

---
+++ src/org/drools/guvnor/decisiontable/client/guvnor/SortDirection.java
@@ -0,0 +1,11 @@
+package org.drools.guvnor.decisiontable.client.guvnor;
+
+/**
+ * @author Geoffrey De Smet
+ */
+public enum SortDirection {
+
+    NONE,
+    ASCENDING,
+    DESCENDING;
+}
---
+++ src/org/drools/guvnor/decisiontable/client/guvnor/TableImageResources.java
@@ -0,0 +1,26 @@
+package org.drools.guvnor.decisiontable.client.guvnor;
+
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
+
+/**
+ * @author Geoffrey De Smet
+ */
+public interface TableImageResources extends ClientBundle {
+
+    @Source("downArrow.png")
+    ImageResource downArrow();
+
+    @Source("smallDownArrow.png")
+    ImageResource smallDownArrow();
+
+    @Source("upArrow.png")
+    ImageResource upArrow();
+
+    @Source("smallUpArrow.png")
+    ImageResource smallUpArrow();
+
+    @Source("columnPicker.png")
+    ImageResource columnPicker();
+
+}
